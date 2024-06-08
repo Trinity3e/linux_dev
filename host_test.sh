9b2fa9c3b519f33cbd06e12f3979bbb1172d729f
Oracle Public Cloud User <opc@ns3-cgii-ro.sub06260725170.vcn1.oraclevcn.com>
2024-06-08 06:43:07 +0000

    Update utilitar hosts

--- host_test.sh
+++ host_test.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 while [[ -f hosts.txt ]]; do cat hosts.txt | while read -r h; do
-status=$(curl -s -L --output /tmp/title --write-out "%{http_code}" "$h" 2>/dev/null | head -n1 | tr -dc '0-9')
-[[ $status -gt 400 ]] && status=$(curl -A 'Mozilla/5.0' -s -L --output /tmp/title --write-out "%{http_code}" "$h" 2>/dev/null | head -n1 | tr -dc '0-9')
+status=$(curl --retry 2 --retry-delay 3 --retry-all-errors -s -L --output /tmp/title --write-out "%{http_code}" "$h" 2>/dev/null | head -n1 | tr -dc '0-9')
+[[ $status -gt 400 ]] && status=$(curl --retry 2 --retry-delay 3 --retry-all-errors -A 'Mozilla/5.0' -s -L --output /tmp/title --write-out "%{http_code}" "$h" 2>/dev/null | head -n1 | tr -dc '0-9')
 shortlink=$(tail -c20 <<<"$h")
 if [[ $status -lt 400 ]] && [[ $status -gt 100 ]]; then
 echo -ne "\n$shortlink UP [$status]"; if [ "$1" = "title" ]; then sed -n -e 's!.*<title>\(.*\)</title>.*!\1!p' /tmp/title | sed 's/^.*$/ [ title: & ]/g'; strings /tmp/title | head -n1 | grep -vi 'html' | head -c25 | sed 's/^.*$/ [ content: & ]/g'; fi | tr -d '\n'; else
-echo -ne "\n$shortlink DOWN $([[ -n $status ]] && ! [ "$status" = '000' ] && echo [$status])"; fi
-done; sleep 3; clear; done
+text="\n$shortlink DOWN $([[ -n $status ]] && ! [ "$status" = '000' ] && echo [$status])"; echo -ne $text; echo -ne $text >> /tmp/errh; ! diff -q <(tail -n1 /tmp/errh) <(echo -ne $text) >/dev/null && mail -s "$(tr '[!@#$%^&*({})|+]' ' ' <<<$shortlink) down" mail@mail.xyz <<<$text; fi
+done; sleep 5; clear; done
