c579985f77eb6e915ed2f8bd93e1c8be54fa0485
Oracle Public Cloud User <opc@ns3-cgii-ro.sub06260725170.vcn1.oraclevcn.com>
2024-06-11 15:54:14 +0000

    Update utilitar zone

---
+++ zone_gui/conf/runners/list_ns3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+sudo find /var/named -name '*.zone' | xargs basename -s .zone 2>/dev/null
+#grep -o 'file.*\.zone' /etc/named.conf | cut -d'"' -f2 | sed 's/.zone//g'
+
+else
+! sudo grep '$1' /etc/named.conf && echo No such domain && exit 1
+echo sed -ie '/$1/,+5d' /etc/named.conf &&
+echo rm -vf /var/named/'$1'.zone &&
+echo Zone '$1' removed
+sudo systemctl restart named
+sudo systemctl status named
+fi
